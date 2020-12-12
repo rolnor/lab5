4d3ebc66dd589a43d9f1dce6a20c50fbea6d7cf2
Rolf <rolnor-8@student.ltu.se>
2020-12-12 17:40:34 +0100

    task 2

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -5,9 +5,6 @@
 
 //void createObject(Shape* object);
 void changeColor(int colors[4], int val1, int val2, int val3, int val4);
-void pushRectangle(vector<Shape*> &shapes);
-void pushTriangle(vector<Shape*> shapes);
-void pushCircle(vector<Shape*> shapes);
 void testRendering(vector<Shape*> shapes, SDL_Renderer* renderer);
 
 
@@ -22,22 +19,24 @@ int main(int argc, char* args[])
 	SDL_Window* window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-	Point2D test;
+	Point2D point2d;
 	int rgb[4] = { 100,233,245,44 };
-	test.setX(17);
-	test.setY(38);
-	Rectangle rectangle(test, rgb, 35, 46);
-	changeColor(rgb, 22, 33, 44, 55);
-	test.setX(107);
-	test.setY(108);
-	Triangle triangle(test, rgb, 150, 160);
-	Triangle triangle2(test, rgb, 250, 260);
-	shapes.push_back(&triangle2);
 
-
+	Rectangle* recPoint = NULL;
+	Triangle* triPoint = NULL;
+	Circle* cirPoint = NULL;
+	srand(time(NULL));
+	float randomx, randomy, randomh, randomw, randomr, randomb;
 
 	while(!quit)
 	{
+		randomx = rand() % 300;
+		randomy = rand() % 300;
+		randomh = rand() % 200;
+		randomw = rand() % 200;
+		randomb = rand() % 200;
+		randomr = rand() % 100;
+
 		SDL_WaitEvent(&event);
 
 		switch (event.type)
@@ -52,18 +51,30 @@ int main(int argc, char* args[])
 						quit = true;
 						break;
 					case SDLK_c:
-						cout << "circle" << endl;
+						changeColor(rgb, 0, 0, 255, 255);
+						point2d.setX(randomx);
+						point2d.setY(randomy);
+						cirPoint = new Circle(point2d, rgb, randomr);
+						shapes.push_back(cirPoint);
 						break;
 					case SDLK_r:
-						cout << "rectangle" << endl;
-						shapes.push_back(&rectangle);
+						changeColor(rgb, 255, 0, 0, 255);
+						point2d.setX(randomx);
+						point2d.setY(randomy);
+						recPoint = new Rectangle(point2d, rgb, randomw, randomh);
+						shapes.push_back(recPoint);
 						break;
 					case SDLK_t:
-						cout << "triangle" << endl;
-						shapes.push_back(&triangle);
+						changeColor(rgb, 155, 155, 0, 255);
+						point2d.setX(randomx);
+						point2d.setY(randomy);
+						triPoint = new Triangle(point2d, rgb, randomb, randomh);
+						shapes.push_back(triPoint);
 						break;
 					case SDLK_x:
 						cout << "remove shapes" << endl;
+						shapes.clear();
+						shapes.shrink_to_fit();
 						break;
 				}
 		}
@@ -72,62 +83,11 @@ int main(int argc, char* args[])
 		testRendering(shapes, renderer);
 		SDL_RenderPresent(renderer);
 	}
-	//SDL_Delay(2000);
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
+	delete recPoint;
+	delete triPoint;
+	delete cirPoint;
 	return 0;
 }
-
-void pushRectangle(vector<Shape*> &shapes)
-{
-	Point2D test;
-
-	int rgb[4] = { 100,233,245,44 };
-	test.setX(17);
-	test.setY(38);
-//	changeColor(rgb, 0, 0, 255, 155);
-	Rectangle rectangle(test, rgb, 35, 46);
-	shapes.push_back(&rectangle);
-}
-
-void pushTriangle(vector<Shape*> shapes)
-{
-	Point2D test;
-	int rgb[4] = { 22, 33, 44, 55 };
-	test.setX(107);
-	test.setY(108);
-	Triangle triangle(test, rgb, 150, 160);
-	shapes.push_back(&triangle);
-}
-
-void pushCircle(vector<Shape*> shapes)
-{
-	Point2D test;
-	test.setX(100);
-	test.setY(100);
-
-	int rgb[4] = { 100,233,245,44 };
-	Circle circle(test, rgb, 30);
-	test.setX(107);
-	test.setY(108);
-	changeColor(rgb, 22, 33, 44, 55);
-	Triangle triangle(test, rgb, 150, 160);
-	test.setX(17);
-	test.setY(38);
-	changeColor(rgb, 122, 133, 144, 155);
-	Rectangle rectangle(test, rgb, 35, 46);
-	shapes.push_back(&circle);
-	shapes.push_back(&triangle);
-	shapes.push_back(&rectangle);
-}
-
-
-/*
-void changeColor(int colors[4], int val1, int val2, int val3, int val4)
-{
-	colors[0] = val1;
-	colors[1] = val2;
-	colors[2] = val3;
-	colors[3] = val4;
-}*/
